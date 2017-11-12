aaf9033fb5b443d9420429b2676134c676adf5d9
VinnieM <vm_hobbies@yahoo.com>
2017-11-11 19:02:14 -0500

    Update hot_pixel_repair.py

--- hot_pixel_repair.py
+++ hot_pixel_repair.py
@@ -32,32 +32,32 @@ from piexif import transplant
 # stops low contrast image warnings from cluttering output.
 warnings.filterwarnings("ignore")
 
-orgdir = './originals/' # directory containing files to repair.
-repdir = './repaired/' # directory containing repaired files
-mskdir = './masks/' # directory containing mask files and pixel maps
+orgdir = './originals/'  # directory containing files to repair.
+repdir = './repaired/'  # directory containing repaired files
+mskdir = './masks/'  # directory containing mask files and pixel maps
 
 parser = argparse.ArgumentParser()
 
-# you must provide a mask file name from those in the 'masks' subfolder.
+# you must provide a mask file name from the 'masks' sub-folder.
 parser.add_argument('-m', '--maskfilename',
                     help='name of jpg mask file',
                     required=True)
 
-# you may provide the file name of the jpg that you want repaired.  It must be
-# placed in the the 'originals' subfolder.  Repaired files are generated in 
-# 'repaired' subfolder. if you don't provide a filename, 
-# all files in the 'originals' subfolder will be repaired. 
+# you may also provide the file name of the jpg that you want repaired.  It must be
+# placed in the the 'originals' sub-folder.  Repaired files are placed in
+# 'repaired' sub-folder. if you don't provide a filename,
+# all files in the 'originals' sub-folder will be repaired.
 parser.add_argument('-f', '--jpgfilename',
                     help='name of jpg file needing hot pixel repair',
                     required=False, default='None')
 
-# parse the input mask file name and optional jpg file needing repair.
+# parse the input mask file name and optional jpg file you want to repair.
 args = parser.parse_args()
 maskfilename = args.maskfilename
 imgfile = args.jpgfilename
 
 # read in pixel map from text file that matches mask file name.
-# the pixel map text file is created by hot_pixel_remapper.py
+# the pixel map text file is created using hot_pixel_remapper.py
 pixel_map = []
 with open(mskdir + maskfilename + '.txt', 'r') as f:
     lines = f.read().splitlines()
@@ -65,41 +65,35 @@ with open(mskdir + maskfilename + '.txt', 'r') as f:
         pnts = line.split(':')
         orgpix = pnts[0].split(',')
         reppix = pnts[1].split(',')               
-        if (orgpix[0].isdigit() and
-            orgpix[1].isdigit() and
-            reppix[0].isdigit() and
-            reppix[1].isdigit()):
-            pixel_map.append([(int(orgpix[0]),int(orgpix[1])),
-                              (int(reppix[0]),int(reppix[1]))])
+        if orgpix[0].isdigit() and orgpix[1].isdigit() and reppix[0].isdigit() and reppix[1].isdigit():
+            pixel_map.append([(int(orgpix[0]), int(orgpix[1])),
+                              (int(reppix[0]), int(reppix[1]))])
         else:
             print('Error = {}'.format(line))
 
 # if you supply a filename, only that file will be repaired.  The file must be 
-# placed in the 'originals' folder.
+# located in the 'originals' folder.
 if imgfile != 'None':
 
     # make sure the file has a jpg or jpeg extension
     if imgfile.lower().endswith(('.jpg', '.jpeg')):
         
         # check that repair file does not already exist.
-        if not isfile(repdir + imgfile.replace('.', 'a.')):
+        if not isfile(repdir + imgfile.replace('.', 'r.')):
     
             startTime = datetime.now()
         
             img = imread(orgdir + imgfile)
             for pix in pixel_map:
-                img[pix[0][0],pix[0][1],0] = img[pix[1][0],pix[1][1],0]
-                img[pix[0][0],pix[0][1],1] = img[pix[1][0],pix[1][1],1]
-                img[pix[0][0],pix[0][1],2] = img[pix[1][0],pix[1][1],2]
-               
+                img[pix[0][0], pix[0][1]] = img[pix[1][0], pix[1][1]]
             
-            # save file with an 'a' at the end of the filename to indicate 
+            # save file with an 'r' at the end of the filename to indicate
             # it was repaired. quality=98 appears to create a file of 
             # approx the same size as the original.
-            imsave(repdir + imgfile.replace('.', 'a.'), img, quality=98)
+            imsave(repdir + imgfile.replace('.', 'r.'), img, quality=98)
         
             # copy the EXIF data from the original file to the repaired file.
-            transplant(orgdir + imgfile, repdir + imgfile.replace('.', 'a.'))
+            transplant(orgdir + imgfile, repdir + imgfile.replace('.', 'r.'))
             
             elapsed_time = datetime.now() - startTime   
             
@@ -114,14 +108,14 @@ if imgfile != 'None':
         s = 'Error: File {} does not end with .jpg or jpeg'
         print(s.format(repdir + imgfile))
     
-else: # repair all files in the 'originals' folder.
+else:  # repair all files in the 'originals' folder.
 
     numfilesrepaired = 0
       
     s = 'mask file = {}, num of pixel remappings = {}'
     print(s.format(maskfilename, len(pixel_map)))
     
-    # get list of files in the 'originals' subfolder
+    # get list of files in the 'originals' sub-folder
     onlyfiles = [f for f in listdir(orgdir) if isfile(join(orgdir, f))]
     
     for imgfile in onlyfiles:
@@ -130,22 +124,20 @@ else: # repair all files in the 'originals' folder.
         if imgfile.lower().endswith(('.jpg', '.jpeg')):
             
             # check that repair file does not already exist.
-            if not isfile(repdir + imgfile.replace('.', 'a.')):
+            if not isfile(repdir + imgfile.replace('.', 'r.')):
         
                 startTime = datetime.now()
                 
                 img = imread(orgdir + imgfile)
                 for pix in pixel_map:
-                    img[pix[0][0],pix[0][1],0] = img[pix[1][0],pix[1][1],0]
-                    img[pix[0][0],pix[0][1],1] = img[pix[1][0],pix[1][1],1]
-                    img[pix[0][0],pix[0][1],2] = img[pix[1][0],pix[1][1],2]
+                    img[pix[0][0], pix[0][1]] = img[pix[1][0], pix[1][1]]
                    
-                # save repaired file in 'repaired' subfolder
-                imsave(repdir + imgfile.replace('.', 'a.'), img, quality=98)
+                # save repaired file in 'repaired' sub-folder
+                imsave(repdir + imgfile.replace('.', 'r.'), img, quality=98)
                 
                 # copy EXIF data from original file to repaired file.
                 transplant(orgdir + imgfile, repdir + imgfile.replace('.',
-                                                                      'a.'))
+                                                                      'r.'))
             
                 elapsed_time = datetime.now() - startTime
             
@@ -155,7 +147,8 @@ else: # repair all files in the 'originals' folder.
                 numfilesrepaired += 1
             else:
                 s = 'Error: File {} already exists'
-                print(s.format(repdir + imgfile.replace('.', 'a.')))                
+                print(s.format(repdir + imgfile.replace('.', 'r.')))
     
     s = 'Done! Number of files repaired = {}'
-    print(s.format(numfilesrepaired))   
+    print(s.format(numfilesrepaired))
+    
